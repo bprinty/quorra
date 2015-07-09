de7f6736bbdcf462b6d11cdc4d3446df19c10298
bprinty <bprinty@gmail.com>
2015-07-09 02:44:25 -0500

    made scaffolding for quorra object, included method for generating scatter plot.

---
+++ src/quorra.js
@@ -0,0 +1,241 @@
+/*
+ * Reusable visualization library.
+ *
+ * @author Blake Printy <bprinty@gmail.com>
+ * ----------------------------------------------- */
+
+
+(function() {
+    "use strict";
+
+    function quorra() {
+        /**
+        quorra()
+
+        Base class for all visualization components, as to not pollute the 
+        global namespace.
+
+        This is where it all begins ... Fight for the Users!
+        */
+    }
+
+    // random number generation with seeding
+    var seed = Math.round(Math.random()*100000);
+    quorra.seed = function(value){
+        if (!arguments.length) return seed;
+        seed = value;
+    }
+    quorra.random = function() {
+        if (typeof seed == "undefined") seed = 42;
+        var x = Math.sin(seed++) * 10000;
+        return x - Math.floor(x);
+    }
+
+    // scatter plot
+    quorra.scatter = function() {
+        /**
+        quorra.scatter()
+
+        Scatter plot. Code for generating this type of plot was inspired from:
+        http://bl.ocks.org/mbostock/3887118
+        */
+
+        // attributes
+        var width = "auto";
+        var height = "auto";
+        var margin = {"top": 20, "bottom": 20, "left": 30, "right": 20};
+        var color = d3.scale.category10();
+        var x = function(d, i) { return d.x; };
+        var y = function(d, i) { return d.y; };
+        var group = function(d, i){ return (typeof d.group == "undefined") ? 0 : d.group; };
+        var label = function(d, i){ return (typeof d.label == "undefined") ? i : d.label; };
+        var size = function(d, i){ return (typeof d.size == "undefined") ? 5 : d.size; };
+        var shape = function(d, i){ return (typeof d.shape == "undefined") ? "circle" : d.shape; };
+        var lm = "smooth";
+        var xlabel = "x";
+        var ylabel = "y";
+        var legend = true;
+        var tooltip = d3.select("body").append("div")
+                .attr("class", "tooltip")
+                .style("position", "absolute")
+                .style("opacity", 0);
+
+        // generator
+        function go(selection){
+            // format selection
+            if (typeof selection == "string") selection = d3.select(selection);
+
+            // if height/width are auto, determine them from selection
+            var w = (width == "auto") ? (parseInt(selection.style("width")) - margin.left - margin.right) : width;
+            var h = (height == "auto") ? (parseInt(selection.style("height")) - margin.top - margin.bottom) : height;
+            
+            // configure axes
+            var xScale = d3.scale.linear().range([0, width]);
+            var yScale = d3.scale.linear().range([height, 0]);
+            var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+            var yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+            // initialize canvas
+            var svg = selection.append("svg")
+                .attr("class", "quorra-scatter")
+                .attr("width", w + margin.left + margin.right)
+                .attr("height", h + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            // axes
+            xScale.domain(d3.extent(data, x)).nice();
+            yScale.domain(d3.extent(data, y)).nice();
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + h + ")")
+                .call(xAxis)
+            .append("text")
+                .attr("class", "label")
+                .attr("x", w)
+                .attr("y", -6)
+                .style("text-anchor", "end")
+                .text(xlabel);
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+            .append("text")
+                .attr("class", "label")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text(ylabel)
+
+            // plotting points
+            svg.selectAll(".dot")
+                .data(svg.data()[0])
+                .enter().append("circle")
+                .attr("class", "dot")
+                .attr("r", size)
+                .attr("cx", function(d, i) { return xScale(x(d, i)); })
+                .attr("cy", function(d, i) { return yScale(y(d, i)); })
+                .style("fill", function(d, i) { return color(group(d, i)); })
+                .style("opacity", 0.75)
+                .on("mouseover", function(d, i){
+                    d3.select(this).style("opacity", 0.25);
+                    tooltip.html(label(d, i))
+                        .style("opacity", 1)
+                        .style("left", (d3.event.pageX + 5) + "px")
+                        .style("top", (d3.event.pageY - 20) + "px");
+                }).on("mousemove", function(d){
+                    tooltip
+                        .style("left", (d3.event.pageX + 5) + "px")
+                        .style("top", (d3.event.pageY - 20) + "px");
+                }).on("mouseout", function(d){
+                    d3.select(this).style("opacity", 0.75);
+                    tooltip.style("opacity", 0);
+                });
+
+            // legend (if specified)
+            if (legend) {
+                var leg = svg.selectAll(".legend")
+                    .data(color.domain())
+                    .enter().append("g")
+                    .attr("class", "legend")
+                    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+                leg.append("rect")
+                    .attr("x", width - 18)
+                    .attr("width", 18)
+                    .attr("height", 18)
+                    .style("fill", color);
+
+                leg.append("text")
+                    .attr("x", width - 24)
+                    .attr("y", 9)
+                    .attr("dy", ".35em")
+                    .style("text-anchor", "end")
+                    .text(function(d) { return d; });
+            }
+        }
+
+        // getters/setters
+        go.width = function(value) {
+            if (!arguments.length) return width;
+            width = value;
+            return go;
+        };
+        go.height = function(value) {
+            if (!arguments.length) return height;
+            height = value;
+            return go;
+        };
+        go.margin = function(value) {
+            if (!arguments.length) return margin;
+            margin = value;
+            return go;
+        };
+        go.color = function(value) {
+            if (!arguments.length) return color;
+            color = value;
+            return go
+        }
+        go.x = function(value) {
+            if (!arguments.length) return x;
+            x = value;
+            return go
+        }
+        go.y = function(value) {
+            if (!arguments.length) return y;
+            y = value;
+            return go
+        }
+        go.group = function(value) {
+            if (!arguments.length) return group;
+            group = value;
+            return go
+        }
+        go.label = function(value) {
+            if (!arguments.length) return label;
+            label = value;
+            return go
+        }
+        go.size = function(value) {
+            if (!arguments.length) return size;
+            size = value;
+            return go
+        }
+        go.shape = function(value) {
+            if (!arguments.length) return shape;
+            shape = value;
+            return go
+        }
+        go.lm = function(value) {
+            if (!arguments.length) return lm;
+            lm = value;
+            return go
+        }
+        go.xlabel = function(value) {
+            if (!arguments.length) return xlabel;
+            xlabel = value;
+            return go
+        }
+        go.ylabel = function(value) {
+            if (!arguments.length) return ylabel;
+            ylabel = value;
+            return go
+        }
+        go.legend = function(value) {
+            if (!arguments.length) return legend;
+            legend = value;
+            return go
+        }
+        go.tooltip = function(value) {
+            if (!arguments.length) return tooltip;
+            tooltip = value;
+            return go
+        }
+
+        return go;
+    };
+
+    // expose internals to global namespace
+    window.quorra = quorra;
+})();
