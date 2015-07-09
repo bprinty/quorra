9c88a3628b9cde3ea76b5fb5387e6aeade426ea1
bprinty <bprinty@gmail.com>
2015-07-09 17:46:28 -0500

    added line plot and examples

--- src/quorra.js
+++ src/quorra.js
@@ -467,11 +467,282 @@
 
         return go;
     };
+    
+    // line plot
+        quorra.line = function() {
+        /**
+        quorra.line()
+
+        Line plot. Code for generating this type of plot was inspired from:
+        http://bl.ocks.org/mbostock/3883245
+        */
+
+        // attributes
+        var width = "auto";
+        var height = "auto";
+        var margin = {"top": 20, "bottom": 20, "left": 30, "right": 20};
+        var color = d3.scale.category10();
+        var x = function(d, i) { return d.x; };
+        var y = function(d, i) { return d.y; };
+        var group = function(d, i){ return (typeof d.group == "undefined") ? 0 : d.group; };
+        var label = function(d, i){ return (typeof d.label == "undefined") ? i : d.label; };
+        var grid = false;
+        var xlabel = "x";
+        var ylabel = "y";
+        var legend = true;
+        var points = 0;
+        var xticks = "auto";
+        var yticks = "auto";
+        var tooltip = d3.select("body").append("div")
+                .attr("id", "scatter-tooltip")
+                .attr("class", "tooltip")
+                .style("position", "absolute")
+                .style("opacity", 0);
+
+        // generator
+        function go(selection){
+            // format selection
+            if (typeof selection == "string") selection = d3.select(selection);
+
+            // if height/width are auto, determine them from selection
+            var w = (width == "auto") ? (parseInt(selection.style("width")) - margin.left - margin.right) : width;
+            var h = (height == "auto") ? (parseInt(selection.style("height")) - margin.top - margin.bottom) : height;
+            
+            // configure axes
+            if (typeof selection.data()[0][0].x == 'string') {
+                var xGroups = _.unique(_.map(selection.data()[0], function(d){ return d.x; }));
+                var xScale = d3.scale.ordinal().range([w/(xGroups.length+2), w - w/(xGroups.length+2)]);
+                xScale.domain(xGroups);
+            }else{
+                var xScale = d3.scale.linear().range([0, w]);
+                xScale.domain(d3.extent(data, x)).nice();
+            }
+            if (typeof selection.data()[0][0].y == 'string') {
+                var yGroups = _.unique(_.map(selection.data()[0], function(d){ return d.y; }));
+                var yScale = d3.scale.ordinal().range([h - h/(yGroups.length+2), h/(yGroups.length+2)]);
+                yScale.domain(yGroups);
+            }else{
+                var yScale = d3.scale.linear().range([h, 0]);
+                yScale.domain(d3.extent(data, y)).nice();
+            }
+            var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+            if (xticks != "auto") {
+                xAxis = xAxis.ticks(xticks);
+            }
+            var yAxis = d3.svg.axis().scale(yScale).orient("left");
+            if (yticks != "auto") {
+                yAxis = yAxis.ticks(yticks);
+            }
+
+            // initialize canvas
+            var svg = selection.append("svg")
+                .attr("class", "quorra-line")
+                .attr("width", w + margin.left + margin.right)
+                .attr("height", h + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            // gridlines (if specified)
+            if (grid){
+                xAxis = xAxis.tickSize(-h, 0, 0);
+                yAxis = yAxis.tickSize(-w, 0, 0);
+            }
+
+            // axes
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + h + ")")
+                .call(xAxis)
+            .append("text")
+                .attr("class", "label")
+                .attr("x", w)
+                .attr("y", -6)
+                .style("text-anchor", "end")
+                .text(xlabel);
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+            .append("text")
+                .attr("class", "label")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text(ylabel)
+
+            // plotting lines
+            var line = d3.svg.line()
+                .x(function(d, i) { return xScale(x(d, i)); })
+                .y(function(d, i) { return yScale(y(d, i)); });
+            
+            var ugrps = _.unique(_.map(selection.data()[0], function(d){ return d.group; }));
+            for (var grp in ugrps) {
+                
+                // lines
+                var subdat = _.filter(selection.data()[0], function(d){ return d.group == ugrps[grp]; });
+                svg.append("path")
+                    .datum(subdat)
+                    .attr("class", "line")
+                    .attr("d", line)
+                    .style("stroke", color(ugrps[grp]))
+                    .style("opacity", 0.75)
+                    .on("mouseover", function(d, i){
+                        d3.select(this).style("opacity", 0.25);
+                        tooltip.html(d[0].group)
+                            .style("opacity", 1)
+                            .style("left", (d3.event.pageX + 5) + "px")
+                            .style("top", (d3.event.pageY - 20) + "px");
+                    }).on("mousemove", function(d){
+                        tooltip
+                            .style("left", (d3.event.pageX + 5) + "px")
+                            .style("top", (d3.event.pageY - 20) + "px");
+                    }).on("mouseout", function(d){
+                        d3.select(this).style("opacity", 0.75);
+                        tooltip.style("opacity", 0);
+                    });
+
+            }
+
+            // points (if specified)
+            if (points > 0){
+                svg.selectAll(".dot")
+                    .data(selection.data()[0])
+                    .enter().append("circle")
+                    .attr("class", "dot")
+                    .attr("r", points)
+                    .attr("cx", function(d, i) { return xScale(x(d, i)); })
+                    .attr("cy", function(d, i) { return yScale(y(d, i)); })
+                    .style("fill", function(d, i){ return color(group(d, i)); })
+                    .style("opacity", 0.75)
+                    .on("mouseover", function(d, i){
+                        d3.select(this).style("opacity", 0.25);
+                        tooltip.html(label(d, i))
+                            .style("opacity", 1)
+                            .style("left", (d3.event.pageX + 5) + "px")
+                            .style("top", (d3.event.pageY - 20) + "px");
+                    }).on("mousemove", function(d){
+                        tooltip
+                            .style("left", (d3.event.pageX + 5) + "px")
+                            .style("top", (d3.event.pageY - 20) + "px");
+                    }).on("mouseout", function(d){
+                        d3.select(this).style("opacity", 0.75);
+                        tooltip.style("opacity", 0);
+                    });
+            }
+
+            // legend (if specified)
+            if (legend) {
+                var leg = svg.selectAll(".legend")
+                    .data(color.domain())
+                    .enter().append("g")
+                    .attr("class", "legend")
+                    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+                leg.append("rect")
+                    .attr("x", w - 18)
+                    .attr("width", 18)
+                    .attr("height", 18)
+                    .style("fill", color);
+
+                leg.append("text")
+                    .attr("x", w - 24)
+                    .attr("y", 9)
+                    .attr("dy", ".35em")
+                    .style("text-anchor", "end")
+                    .text(function(d) { return d; });
+            }
+        }
+
+        // getters/setters
+        go.width = function(value) {
+            if (!arguments.length) return width;
+            width = value;
+            return go;
+        };
+        go.height = function(value) {
+            if (!arguments.length) return height;
+            height = value;
+            return go;
+        };
+        go.margin = function(value) {
+            if (!arguments.length) return margin;
+            margin = value;
+            return go;
+        };
+        go.color = function(value) {
+            if (!arguments.length) return color;
+            color = value;
+            return go
+        }
+        go.x = function(value) {
+            if (!arguments.length) return x;
+            x = value;
+            return go
+        }
+        go.y = function(value) {
+            if (!arguments.length) return y;
+            y = value;
+            return go
+        }
+        go.group = function(value) {
+            if (!arguments.length) return group;
+            group = value;
+            return go
+        }
+        go.label = function(value) {
+            if (!arguments.length) return label;
+            label = value;
+            return go
+        }
+        go.points = function(value) {
+            if (!arguments.length) return points;
+            points = value;
+            return go  
+        }
+        go.grid = function(value) {
+            if (!arguments.length) return grid;
+            grid = value;
+            return go
+        }
+        go.xticks = function(value) {
+            if (!arguments.length) return xticks;
+            xticks = value;
+            return go
+        }
+        go.yticks = function(value) {
+            if (!arguments.length) return yticks;
+            yticks = value;
+            return go
+        }
+        go.xlabel = function(value) {
+            if (!arguments.length) return xlabel;
+            xlabel = value;
+            return go
+        }
+        go.ylabel = function(value) {
+            if (!arguments.length) return ylabel;
+            ylabel = value;
+            return go
+        }
+        go.legend = function(value) {
+            if (!arguments.length) return legend;
+            legend = value;
+            return go
+        }
+        go.tooltip = function(value) {
+            if (!arguments.length) return tooltip;
+            tooltip = value;
+            return go
+        }
+
+        return go;
+    };
+
+    // density plot
     // bar
     // histogram
-    // density plot
     // boxplot
-    // line plot
     // venn diagram
 
     // expose internals to global namespace
